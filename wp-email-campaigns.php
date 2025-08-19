94757d082432b0b9494b9fb871acfba8416ff439
laukika-in <tech@laukika.com>
2025-08-19 15:05:02 +0530

    Update wp-email-campaigns.php

--- wp-email-campaigns.php
+++ wp-email-campaigns.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       WP Email Campaigns
  * Description:       Transactional email campaigns via CPT with Excel/CSV import, Action Scheduler (1 email/3s), contacts, and reporting.
- * Version:           1.3.3
+ * Version:           1.3.4
  * Author:            Anirudh
  * Text Domain:       wp-email-campaigns
  */
