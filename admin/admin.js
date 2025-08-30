ebea19bc69e29ee1a9a68692cd6a34cfc746854a
laukika-in <tech@laukika.com>
2025-08-30 23:02:24 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -2155,7 +2155,13 @@
           if (ix === -1) return null;
           const id = pair.slice(0, ix);
           const name = pair.slice(ix + 2);
-          return '<a href="' + base + id + '">' + escapeHtml(name) + "</a>";
+          return (
+            '<a href="edit.php?page=wpec-lists&view=list&list_id=' +
+            id +
+            '">' +
+            escapeHtml(name) +
+            "</a>"
+          );
         })
         .filter(Boolean);
 
