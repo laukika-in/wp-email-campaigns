64dd61c73245cb2cb360791e010bfed992df51e3
laukika-in <tech@laukika.com>
2025-08-30 17:38:16 +0530

    some changes

--- includes/class-contacts.php
+++ includes/class-contacts.php
@@ -424,6 +424,8 @@ class Contacts {
         echo '<th>'.esc_html__('Full name','wp-email-campaigns').'</th>';
         echo '<th>'.esc_html__('Email','wp-email-campaigns').'</th>';
         echo '<th>'.esc_html__('List(s)','wp-email-campaigns').'</th>';
+        echo '<th>'.esc_html__('Status','wp-email-campaigns').'</th>';
+        echo '<th>'.esc_html__('Created','wp-email-campaigns').'</th>';
         echo '</tr></thead>';
 
         echo '<tbody></tbody>';
